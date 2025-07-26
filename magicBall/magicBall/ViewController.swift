f3017e9bcc4c5450dd4258516c1751d37d77c3bf
luichemo <luka.chemia001@gmail.com>
2025-07-26 18:33:40 +0400

    delete - delete unused file

--- magicBall/magicBall/ViewController.swift
+++ magicBall/magicBall/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  magicBall
-//
-//  Created by Luka on 26.07.25.
-//
-
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    let ballImages: [UIImage] = [
+        UIImage(named: "ball1.png")!,
+        UIImage(named: "ball2.png")!,
+        UIImage(named: "ball3.png")!,
+        UIImage(named: "ball4.png")!,
+        UIImage(named: "ball5.png")!,
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            shakeBall()
+        }
+    }
+    
+    func shakeBall() {
+        ballImageView.image = ballImages.randomElement()
+    }
 }
-
